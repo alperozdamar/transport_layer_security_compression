cf49572c0edc3852be8c5d39919172ccf5780e40
Rozita <rozita.teim@gmail.com>
2019-02-23 23:44:33 -0800

    Implement 4 node ... WIP ...

--- ns-3-allinone/ns-3-dev/scratch/point2point.cc
+++ ns-3-allinone/ns-3-dev/scratch/point2point.cc
@@ -12,8 +12,23 @@ NS_LOG_COMPONENT_DEFINE("Point to point conection");
 int 
 main (int argc, char *argv[])
 {
-  //CommandLine cmd;
-  //cmd.Parse (args, argh);
+
+  NS_LOG_INFO ("Command Line Argument!");
+  CommandLine cmd;
+  cmd.AddValue ("useIpv6", "Use Ipv6", useV6);
+  cmd.Parse (argc, argv);
+
+  NS_LOG_INFO ("Decleare variable!");
+  bool useV6 = false;
+  Address serverAddress;
+  uint16_t port = 9;  
+  uint32_t responseSize = 1100;
+  uint32_t packetSize = 1100;
+  uint32_t maxPacketCount = 2;
+  Time interPacketInterval = Seconds (0);
+
+
+
 
   Time::SetResolution (Time::NS);
   LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
@@ -21,44 +36,91 @@ main (int argc, char *argv[])
 
   // Create Node
   NodeContainer nodes;
-  nodes.Create(2);
+  nodes.Create(4);
+
+  InternetStackHelper stack;
+  stack.Install(nodes);
 
   // Create link between nodes and set attribute
   PointToPointHelper pointToPoint;
+  /// pointToPoint.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
+  /// pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
+
+  //pointToPoint.SetChannelAttribute ("DataRate", DataRateValue (DataRate (5000000)));
   pointToPoint.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
-  pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
+  pointToPoint.SetChannelAttribute ("Delay", TimeValue (MilliSeconds (2)));
+  pointToPoint.SetDeviceAttribute ("Mtu", UintegerValue (1100));
 
   // connect nodes using DeviceContainer
   NetDeviceContainer devices;
   devices = pointToPoint.Install(nodes);
 
+ // NetDeviceContainer deviceNode1_2;
+ // deviceNode1_2 = pointToPoint.Install(nodes);
 
-  InternetStackHelper stack;
-  stack.Install(nodes);
+
+
+  NS_LOG_INFO ("Assign IP Addresses.");
+  if (useV6 == false)
+    {
+      Ipv4AddressHelper ipv4Address;
+      ipv4Address.SetBase ("10.1.1.0", "255.255.255.0");
+      Ipv4InterfaceContainer interfaces = ipv4Address.Assign (devices);
+      serverAddress = Address(interfaces.GetAddress (1));
+    }
+  else
+    {
+      Ipv6AddressHelper ipv6Address;
+      ipv6Address.SetBase ("2001:0000:f00d:cafe::", Ipv6Prefix (64));
+      Ipv6InterfaceContainer interfaces = ipv6Address.Assign (devices);
+      serverAddress = Address(interfaces.GetAddress (1,1));
+    }
 
   // assign IP to nodes and generate interface from node and IP
-  Ipv4AddressHelper address;
-  address.SetBase("10.1.1.0", "255.255.255.0");
-  Ipv4InterfaceContainer interfaces = address.Assign (devices);
+  ///Ipv4AddressHelper address;
+  ///address.SetBase("10.1.1.0", "255.255.255.0");
+  ///Ipv4InterfaceContainer interfaces = address.Assign (devices);
+
+  NS_LOG_INFO ("Create Applications.");
+  
+  UdpEchoServerHelper echoServer (port);
+  echoServer.SetAttribute ("PacketSize", UintegerValue (responseSize));
+  ApplicationContainer serverApps = echoServer.Install (nodes.Get (1));
+  serverApps.Start (Seconds (1.0));
+  serverApps.Stop (Seconds (10.0));
+
+
 
   // set server
-  UdpEchoServerHelper echoServer(9);
+  ///UdpEchoServerHelper echoServer(9);
 
   // create application on top of server
-  ApplicationContainer serverApps = echoServer.Install(nodes.Get(1));
-  serverApps.Start(Seconds(1.0));
-  serverApps.Stop(Seconds(10.0));
+  ///ApplicationContainer serverApps = echoServer.Install(nodes.Get(1));
+  ///serverApps.Start(Seconds(1.0));
+  ///serverApps.Stop(Seconds(10.0));
+
+
+  
+  UdpEchoClientHelper echoClient (serverAddress, port);
+  echoClient.SetAttribute ("MaxPackets", UintegerValue (maxPacketCount));
+  echoClient.SetAttribute ("Interval", TimeValue (interPacketInterval));
+  echoClient.SetAttribute ("PacketSize", UintegerValue (packetSize));
+ 
+  ApplicationContainer clientApps = echoClient.Install(nodes.Get(0));
+  clientApps.Start (Seconds (2.0));
+  clientApps.Stop (Seconds (10.0));
+
 
   // create interface btw client and server, set attribute
-  UdpEchoClientHelper echoClient(interfaces.GetAddress(1), 9);
-  echoClient.SetAttribute("MaxPackets", UintegerValue(1));
-  echoClient.SetAttribute("Interval", TimeValue(Seconds(1.0)));
-  echoClient.SetAttribute("PacketSize", UintegerValue(1024));
+  ///UdpEchoClientHelper echoClient(interfaces.GetAddress(1), 9);
+  ///echoClient.SetAttribute("MaxPackets", UintegerValue(1));
+  ///echoClient.SetAttribute("Interval", TimeValue(Seconds(1.0)));
+  ///echoClient.SetAttribute("PacketSize", UintegerValue(1024));
 
   // create application on top of client
-  ApplicationContainer clientApps = echoClient.Install(nodes.Get(0));
-  clientApps.Start(Seconds(2.0));
-  clientApps.Stop(Seconds(10.0));
+  ///ApplicationContainer clientApps = echoClient.Install(nodes.Get(0));
+  ///clientApps.Start(Seconds(2.0));
+  ///clientApps.Stop(Seconds(10.0));
 
   Simulator::Run ();
   Simulator::Destroy ();
