5ade12f73efa67de337088b5f7026db54e9c8f75
Rozita <rozita.teim@gmail.com>
2019-02-28 14:08:42 -0800

    fix protocol number

--- ns-3-allinone/ns-3-dev/src/point-to-point/model/ppp-header.cc
+++ ns-3-allinone/ns-3-dev/src/point-to-point/model/ppp-header.cc
@@ -67,6 +67,9 @@ PppHeader::Print (std::ostream &os) const
     case 0x0057: /* IPv6 */
       proto = "IPv6 (0x0057)";
       break;
+    case 0x4021:
+      proto = "Compress (Ox4021)";
+      break;
     default:
       NS_ASSERT_MSG (false, "PPP Protocol number not defined!");
     }
